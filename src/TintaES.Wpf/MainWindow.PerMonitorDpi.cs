d969ff08979d373b9e8a83ffa88f1481aa14162f
treska23 <treska23@hotmail.com>
2026-07-24 13:24:02 +0200

    Fix WPF control imports for responsive DPI layout

--- src/TintaES.Wpf/MainWindow.PerMonitorDpi.cs
+++ src/TintaES.Wpf/MainWindow.PerMonitorDpi.cs
@@ -1,5 +1,6 @@
 using System.Runtime.InteropServices;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Threading;
