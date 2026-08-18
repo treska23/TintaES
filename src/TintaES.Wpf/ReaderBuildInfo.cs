f380a4a4247bdb601434c665424eedb73a82de5f
treska23 <treska23@hotmail.com>
2026-08-19 00:31:32 +0200

    Mark immersive touch reader build

--- src/TintaES.Wpf/ReaderBuildInfo.cs
+++ src/TintaES.Wpf/ReaderBuildInfo.cs
@@ -6,5 +6,5 @@ namespace TintaES.Wpf;
 /// </summary>
 internal static class ReaderBuildInfo
 {
-    internal const string CurrentBuildStamp = "Reader 2026.08.19-r4-touch-hold";
+    internal const string CurrentBuildStamp = "Reader 2026.08.19-r5-immersive-touch";
 }
